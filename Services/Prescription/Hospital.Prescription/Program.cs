688375d270ab78ff0cda3314136bbb59270dd5a1
edaagunes <edagunes00@hotmail.com>
2025-03-18 15:57:18 +0300

    Added Redis configuration

--- Services/Prescription/Hospital.Prescription/Program.cs
+++ Services/Prescription/Hospital.Prescription/Program.cs
@@ -1,8 +1,45 @@
+using Hospital.Prescription.LoginServices;
+using Hospital.Prescription.Services;
+using Hospital.Prescription.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Options;
+using System.IdentityModel.Tokens.Jwt;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
 
-builder.Services.AddControllers();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+{
+	opt.Authority = builder.Configuration["IdentityServerUrl"];
+	opt.Audience = "ResourcePrescription";
+	opt.RequireHttpsMetadata = false;
+});
+
+builder.Services.AddControllers(opt =>
+{
+	opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddScoped<ILoginService,LoginService>();
+builder.Services.AddScoped<IPrescriptionService,PrescriptionService>();
+
+builder.Services.Configure<RedisSettings>(builder.Configuration.GetSection("RedisSettingsKey"));
+builder.Services.AddSingleton<RedisService>(sp =>
+{
+	var redisSettings=sp.GetRequiredService<IOptions<RedisSettings>>().Value;
+	var redis=new RedisService(redisSettings.Host, redisSettings.Port);
+	redis.Connect();
+	return redis;
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -17,6 +54,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
--- Services/Prescription/Hospital.Prescription/Services/IPrescriptionService.cs
+++ Services/Prescription/Hospital.Prescription/Services/IPrescriptionService.cs
@@ -4,7 +4,7 @@ namespace Hospital.Prescription.Services
 {
 	public interface IPrescriptionService
 	{
-		Task<PrescriptionTotalDto> GetPrescriptionTotalAsync();
+		Task<PrescriptionTotalDto> GetPrescriptionTotalAsync(string userId);
 		Task SavePrescriptionAsync(PrescriptionTotalDto prescriptionTotal);
 		Task DeletePrescriptionAsync(string userId);
 	}
